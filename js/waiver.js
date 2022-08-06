1d192354dc7eef73f58dd6c0c34c860e20ada4a1
supraditya <aditya300100@gmail.com>
2022-08-06 12:09:27 +0530

    Problem 2 complete

--- js/waiver.js
+++ js/waiver.js
@@ -1,7 +1,13 @@
 // Problem 1
 
-var figures= document.querySelectorAll(".container>figure");
-console.log(figures)
-figures.forEach(figure=>{
-    figure.classList.add("one-third")
-})
+var figures = document.querySelectorAll(".container>figure");
+figures.forEach((figure) => {
+  figure.classList.add("one-third");
+});
+
+// Problem 2
+
+var footer = document.querySelector("footer");
+footer.addEventListener("click", () => {
+  footer.style.display = "none";
+});
