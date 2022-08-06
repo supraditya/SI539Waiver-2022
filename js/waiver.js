bea48fbd531de7a5db3f8dcadf758d11723e547d
supraditya <aditya300100@gmail.com>
2022-08-06 12:12:30 +0530

    Problem 3 Complete

--- js/waiver.js
+++ js/waiver.js
@@ -11,3 +11,10 @@ var footer = document.querySelector("footer");
 footer.addEventListener("click", () => {
   footer.style.display = "none";
 });
+
+// Problem 3
+
+var fifth_figure = document.querySelector(".container>figure:nth-child(5)");
+fifth_figure.addEventListener("dblclick", () => {
+  fifth_figure.style.visibility = "hidden";
+});
